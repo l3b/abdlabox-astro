2b518c3c39f5045d90edae26cd9c973bf3260984
abdla <l3b@Hotmail.com>
2025-04-28 01:17:00 +0000

    Restored to 'bebd8d6ee67d6a7d99d86148a31fdfc9eaa26f2e'
    Replit-Restored-To: bebd8d6ee67d6a7d99d86148a31fdfc9eaa26f2e

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-    port: 5000,
-    hmr: {
-      clientPort: 5000
-    },
-    allowedHosts: [
-      ".replit.dev",
-      "0.0.0.0",
-      "localhost",
-      ".repl.co"
-    ]
+    port: 5000
   }
 });
